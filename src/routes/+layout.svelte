60eeecb1cff52f0ce002ca4dd38e48473346c8c6
savioruz <jakueenak@gmail.com>
2025-02-15 00:55:34 +0700

    Add Vercel Speed Insights and minor dependency

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,10 +6,12 @@
 	import NotFound from '$lib/components/widgets/not-found.svelte';
 	import { dev } from '$app/environment';
 	import { injectAnalytics } from '@vercel/analytics/sveltekit';
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 
 	let { children } = $props();
 	
 	injectAnalytics({ mode: dev ? 'development' : 'production' });
+	injectSpeedInsights();
 </script>
 
 <svelte:head>
