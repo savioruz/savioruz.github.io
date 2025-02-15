6e1cb5dc5741717d128029d3f61cdb936a7f172a
savioruz <jakueenak@gmail.com>
2025-02-15 12:33:38 +0700

    Add dynamic class support to root widget component

--- src/lib/components/widgets/root.svelte
+++ src/lib/components/widgets/root.svelte
@@ -1,6 +1,16 @@
+<script lang="ts">
+	import { cn } from '$lib/utils';
+
+	let className: string = '';
+	export { className as class };
+</script>
+
 <main class="flex min-h-screen flex-col items-center justify-start py-8 md:my-8 md:justify-center">
 	<div
-		class="flex w-full max-w-sm flex-col items-start justify-center gap-20 px-4 md:max-w-screen-md md:px-0"
+		class={cn(
+			"flex w-full max-w-sm flex-col items-start justify-center gap-20 px-4 md:max-w-screen-md md:px-0",
+			className
+		)}
 	>
 		<slot />
 	</div>
